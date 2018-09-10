1ad861685f195e7f98dc8c1483eaf7fe21731c74
Richard Hofman <richard@hofman.co.nz>
2018-09-10 23:40:01 +1000

    Remove settings.h in favour of .example version

--- src/settings.h
+++
@@ -1,9 +0,0 @@
-#define SERIAL_DEBUG    // Comment out to disable Serial output.
-#define PIN 0           // D3 on WEMOS D1 mini.
-#define NUM_PIXELS 60   // Number of pixels on the LED strip.
-
-#define MQTT_SERVER "mqtt.example.com"
-#define MQTT_PORT 1883
-#define MQTT_USERNAME "user"
-#define MQTT_PASSWORD "password"
-#define MQTT_SUB_TOPIC "lights/neopixels/set"
